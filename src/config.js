272409e11c0c41d90fc6a450d2ba3fd53390e211
Dafu Ai <dafua@student.unimelb.edu.au>
2019-07-18 16:25:33 +1000

    fix var name

--- src/config.js
+++ src/config.js
@@ -1,49 +1,49 @@
-import * as config from './config.json';
+import * as CONFIG from './config.json';
 
 const STAGE = process.env.REACT_APP_STAGE;
 
 const base = {
     apiGateway: {
-        REGION: config.main_region,
+        REGION: CONFIG.main_region,
         URL: `https://2a0mvgomd8.execute-api.ap-southeast-2.amazonaws.com/${STAGE}`,
     },
 };
 
 const localhostConfig = {
     cognito: {
-        REGION: config.main_region,
-        USER_POOL_ID: config.cognito_user_pool_id,
-        APP_CLIENT_ID: config.cognito_app_client_id_localhost,
-        IDENTITY_POOL_ID: config.cognito_identity_pool_id,
+        REGION: CONFIG.main_region,
+        USER_POOL_ID: CONFIG.cognito_user_pool_id,
+        APP_CLIENT_ID: CONFIG.cognito_app_client_id_localhost,
+        IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id,
         OAUTH: {
-            domain: `${config.cognito_oauth_domain_localhost}.auth.ap-southeast-2.amazoncognito.com`,
-            scope: config.cognito_oauth_scope,
-            redirectSignIn: config.cognito_oauth_redirect_signin_localhost,
-            redirectSignOut: config.cognito_oauth_redirect_signout_localhost,
-            responseType: config.cognito_oauth_response_type,
+            domain: `${CONFIG.cognito_oauth_domain_localhost}.auth.ap-southeast-2.amazoncognito.com`,
+            scope: CONFIG.cognito_oauth_scope,
+            redirectSignIn: CONFIG.cognito_oauth_redirect_signin_localhost,
+            redirectSignOut: CONFIG.cognito_oauth_redirect_signout_localhost,
+            responseType: CONFIG.cognito_oauth_response_type,
         },
     },
 };
 
 const stageConfig = {
     cognito: {
-        REGION: config.main_region,
-        USER_POOL_ID: config.cognito_user_pool_id,
-        APP_CLIENT_ID: config.cognito_app_client_id_stage,
-        IDENTITY_POOL_ID: config.cognito_identity_pool_id,
+        REGION: CONFIG.main_region,
+        USER_POOL_ID: CONFIG.cognito_user_pool_id,
+        APP_CLIENT_ID: CONFIG.cognito_app_client_id_stage,
+        IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id,
         OAUTH: {
-            domain: config.cognito_oauth_domain_stage,
-            scope: config.cognito_oauth_scope,
-            redirectSignIn: config.cognito_oauth_redirect_signin,
-            redirectSignOut: config.cognito_oauth_redirect_signout,
-            responseType: config.cognito_oauth_response_type,
+            domain: CONFIG.cognito_oauth_domain_stage,
+            scope: CONFIG.cognito_oauth_scope,
+            redirectSignIn: CONFIG.cognito_oauth_redirect_signin,
+            redirectSignOut: CONFIG.cognito_oauth_redirect_signout,
+            responseType: CONFIG.cognito_oauth_response_type,
         },
     },
 };
 
-const config = STAGE === 'localhost' ? localhostConfig : stageConfig;
+const currentConfig = STAGE === 'localhost' ? localhostConfig : stageConfig;
 
 export default {
     ...base,
-    ...config,
+    ...currentConfig,
 };
