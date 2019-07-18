af1423339e913289b3f70bb3a9d08e516fc70f1d
Dafu Ai <dafua@student.unimelb.edu.au>
2019-07-19 09:39:25 +1000

    fix config.js

--- src/config.js
+++ src/config.js
@@ -2,21 +2,25 @@ import * as CONFIG from './config.json';
 
 const STAGE = process.env.REACT_APP_STAGE;
 
+const REGION = CONFIG.main_region.value;
+
 const base = {
     apiGateway: {
-        REGION: CONFIG.main_region.value,
+        REGION: REGION,
         URL: `https://2a0mvgomd8.execute-api.ap-southeast-2.amazonaws.com/${STAGE}`,
     },
 };
 
+const OAUTH_DOMAIN = `${CONFIG.cognito_oauth_domain.value}.auth.${REGION}.amazoncognito.com`;
+
 const localhostConfig = {
     cognito: {
-        REGION: CONFIG.main_region.value,
+        REGION: REGION,
         USER_POOL_ID: CONFIG.cognito_user_pool_id.value,
         APP_CLIENT_ID: CONFIG.cognito_app_client_id_localhost.value,
         IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id.value,
         OAUTH: {
-            domain: `${CONFIG.cognito_oauth_domain.value}.auth.ap-southeast-2.amazoncognito.com`,
+            domain: OAUTH_DOMAIN,
             scope: CONFIG.cognito_oauth_scope.value,
             redirectSignIn: CONFIG.cognito_oauth_redirect_signin_localhost.value,
             redirectSignOut: CONFIG.cognito_oauth_redirect_signout_localhost.value,
@@ -27,12 +31,12 @@ const localhostConfig = {
 
 const stageConfig = {
     cognito: {
-        REGION: CONFIG.main_region.value,
+        REGION: REGION,
         USER_POOL_ID: CONFIG.cognito_user_pool_id.value,
         APP_CLIENT_ID: CONFIG.cognito_app_client_id_stage.value,
         IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id.value,
         OAUTH: {
-            domain: `${CONFIG.cognito_oauth_domain.value}.auth.ap-southeast-2.amazoncognito.com`,
+            domain: OAUTH_DOMAIN,
             scope: CONFIG.cognito_oauth_scope.value,
             redirectSignIn: CONFIG.cognito_oauth_redirect_signin.value,
             redirectSignOut: CONFIG.cognito_oauth_redirect_signout.value,
@@ -42,6 +46,8 @@ const stageConfig = {
 };
 
 const currentConfig = STAGE === 'localhost' ? localhostConfig : stageConfig;
+
+console.log(currentConfig);
 
 export default {
     ...base,
