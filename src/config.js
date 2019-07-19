dece57c769ddf2fe50d761df2d6f2c4099d73301
Dafu Ai <dafua@student.unimelb.edu.au>
2019-07-19 17:40:57 +1000

    changed to use env variables from buildspec

--- src/config.js
+++ src/config.js
@@ -1,55 +1,37 @@
-import * as CONFIG from './config.json';
-
 const STAGE = process.env.REACT_APP_STAGE;
+const REGION = process.env.REACT_APP_REGION;
 
-const REGION = CONFIG.main_region.value;
+// Determine whether current environment is local
+const IS_LOCAL = STAGE === 'localhost';
 
-const base = {
+const OAUTH_DOMAIN = `${process.env.REACT_APP_OAUTH_DOMAIN}.auth.${REGION}.amazoncognito.com`;
+
+const config = {
     apiGateway: {
         REGION: REGION,
         URL: `https://2a0mvgomd8.execute-api.ap-southeast-2.amazonaws.com/${STAGE}`,
     },
-};
-
-const OAUTH_DOMAIN = `${CONFIG.cognito_oauth_domain.value}.auth.${REGION}.amazoncognito.com`;
-
-const localhostConfig = {
     cognito: {
         REGION: REGION,
-        USER_POOL_ID: CONFIG.cognito_user_pool_id.value,
-        APP_CLIENT_ID: CONFIG.cognito_app_client_id_localhost.value,
-        IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id.value,
+        USER_POOL_ID: process.env.REACT_APP_COG_USER_POOL_ID,
+        APP_CLIENT_ID: IS_LOCAL
+            ? process.env.REACT_APP_COG_APP_CLIENT_ID_LOCAL
+            : process.env.REACT_APP_COG_APP_CLIENT_ID_STAGE,
+        IDENTITY_POOL_ID: process.env.REACT_APP_COG_IDENTITY_POOL_ID,
         OAUTH: {
             domain: OAUTH_DOMAIN,
-            scope: CONFIG.cognito_oauth_scope.value,
-            redirectSignIn: CONFIG.cognito_oauth_redirect_signin_localhost.value,
-            redirectSignOut: CONFIG.cognito_oauth_redirect_signout_localhost.value,
-            responseType: CONFIG.cognito_oauth_response_type.value,
+            scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+            redirectSignIn: IS_LOCAL
+                ? process.env.REACT_APP_OAUTH_REDIRECT_IN_LOCAL
+                : process.env.REACT_APP_OAUTH_REDIRECT_IN_STAGE,
+            redirectSignOut: IS_LOCAL
+                ? process.env.REACT_APP_OAUTH_REDIRECT_OUT_LOCAL
+                : process.env.REACT_APP_OAUTH_REDIRECT_OUT_STAGE,
+            responseType: "code",
         },
     },
 };
 
-const stageConfig = {
-    cognito: {
-        REGION: REGION,
-        USER_POOL_ID: CONFIG.cognito_user_pool_id.value,
-        APP_CLIENT_ID: CONFIG.cognito_app_client_id_stage.value,
-        IDENTITY_POOL_ID: CONFIG.cognito_identity_pool_id.value,
-        OAUTH: {
-            domain: OAUTH_DOMAIN,
-            scope: CONFIG.cognito_oauth_scope.value,
-            redirectSignIn: CONFIG.cognito_oauth_redirect_signin.value,
-            redirectSignOut: CONFIG.cognito_oauth_redirect_signout.value,
-            responseType: CONFIG.cognito_oauth_response_type.value,
-        },
-    },
-};
+console.log(config);
 
-const currentConfig = STAGE === 'localhost' ? localhostConfig : stageConfig;
-
-console.log(currentConfig);
-
-export default {
-    ...base,
-    ...currentConfig,
-};
+export default config;
