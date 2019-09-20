7f9753cbe976f57e2c09f9b565467302f86b6907
Dafu Ai <dafua@student.unimelb.edu.au>
2019-09-20 10:41:54 +1000

    try change scope

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ const config = {
         IDENTITY_POOL_ID: process.env.REACT_APP_COG_IDENTITY_POOL_ID,
         OAUTH: {
             domain: OAUTH_DOMAIN,
-            scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+            scope: ["email", "openid", "profile"],
             redirectSignIn: IS_LOCAL
                 ? process.env.REACT_APP_OAUTH_REDIRECT_IN_LOCAL
                 : process.env.REACT_APP_OAUTH_REDIRECT_IN_STAGE,
