feb23738c81f545c589c02a50c7834b9bc2b99b8
Victor San Kho Lin <victor@sankholin.com>
2020-01-29 11:38:46 +1100

    Fixed human readable to decimal metric
    * KB, MB, GB, TB, ...

--- src/components/HumanReadableFileSize.js
+++ src/components/HumanReadableFileSize.js
@@ -3,8 +3,8 @@ import * as PropTypes from 'prop-types';
 
 const suffixes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 const getBytes = (bytes) => {
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return (!bytes && '0 Bytes') || (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + suffixes[i];
+  const i = Math.floor(Math.log(bytes) / Math.log(1000));
+  return (!bytes && '0 Bytes') || (bytes / Math.pow(1000, i)).toFixed(2) + ' ' + suffixes[i];
 };
 
 const HumanReadableFileSize = ({ bytes }) => <span>{getBytes(bytes)}</span>;
