d1050b35d4a650825d3feb73ea9dbc7795131ba4
Roman Valls Guimera <brainstorm@nopcode.org>
2020-12-15 14:31:31 +1100

    npm audit fix https://davejansen.com/audit-fix-your-npm-packages-when-using-yarn/

--- src/containers/IGV.js
+++ src/containers/IGV.js
@@ -66,14 +66,14 @@ class IGV extends Component {
 
   async componentDidMount() {
     const values = queryString.parse(this.props.location.search);
-    if (values.bucket && values.key) {
-      this.directS3IGVAccess(values.bucket, values.key);
-    } else if (values.htsget) {
-      this.htsgetAccess(values.htsget);
-    }
+    //if (values.bucket && values.key) {
+    //  this.directS3IGVAccess(values.bucket, values.key);
+    //} else if (values.htsget) {
+    this.htsgetAccess(values.htsget);
+    //}
   }
 
-  htsgetAccess(htsget_req) {
+  async htsgetAccess(htsget_req) {
     this.setState(
       {
         htsget: htsget_req,
@@ -112,22 +112,28 @@ class IGV extends Component {
   }
 
   renderIgv = () => {
-    //const { filename, signedURL, signedIndexURL, format } = this.state;
     const options = {
-      genome: 'hg38',
+      genome: 'hg19',
+      locus: 'chr8:128,747,267-128,754,546',
       tracks: [
         {
           type: 'alignment',
           sourceType: 'htsget',
-          url: 'https://htsget.dev.umccr.org/',
+          url: 'http://htsget.dev.umccr.org',
           endpoint: '/reads/',
           id: 'tabulamuris.A1-B000126-3_39_F-1-1_R2',
+          name: 'NA12878',
         },
       ],
     };
 
     const igvDiv = document.getElementById('igv-div');
-    igv.createBrowser(igvDiv, options);
+
+    igv.createBrowser(igvDiv, options).then(function (browser) {
+      console.log('Creating IGV browser');
+      igv.browser = browser;
+      console.log('Created IGV browser');
+    });
   };
 
   render() {
