e03f3bae472325d6e1601e5a6f53e184b79e02bc
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-09-20 17:56:13 +1000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,40 +15,39 @@ import LIMSPage from '../pages/LIMS';
 import MenuBar from '../layouts/MenuBar';
 
 function Routes() {
-  return (
-    <RouterRoutes>
-      <Route path='/' element={<ProtectedRoute />}>
-        {/* NoPath redirects to HomePage */}
-        <Route index element={<HomePage />} />
+  const isUserSignedIn = useUserContext().isAuth;
 
+  if (!isUserSignedIn) {
+    return (
+      <RouterRoutes>
         <Route path='/signIn' element={<SignInPage />} />
-        <Route path='/metadata' element={<MetadataPage />} />
-        <Route path='/lims' element={<LIMSPage />} />
+      </RouterRoutes>
+    );
+  } else {
+    return (
+      <RouterRoutes>
+        <Route path='/' element={<SignedInLayout />}>
+          {/* NoPath redirects to HomePage */}
+          <Route index element={<HomePage />} />
 
-        <Route path='/signIn' element={<SignInPage />} />
-        <Route path='/metadata' element={<MetadataPage />} />
-        <Route path='/lims' element={<LIMSPage />} />
+          <Route path='/metadata' element={<MetadataPage />} />
+          <Route path='/lims' element={<LIMSPage />} />
 
-        {/* Complicated routing or more than one routing will be split into their own component. */}
-        <Route path='/subjects/*' element={<SubjectRoutes />} />
+          {/* Complicated routing or more than one routing will be split into their own component. */}
+          <Route path='/subjects/*' element={<SubjectRoutes />} />
 
-        {/* Non matching page redirect to NotFound */}
-        <Route path='*' element={<h1>NotFoundPage</h1>}></Route>
-      </Route>
-      <Route path='/signIn' element={<SignInPage />}></Route>
-    </RouterRoutes>
-  );
+          {/* Non matching page redirect to NotFound */}
+          <Route path='*' element={<h1>NotFoundPage</h1>}></Route>
+        </Route>
+        <Route path='/signIn' element={<SignInPage />}></Route>
+      </RouterRoutes>
+    );
+  }
 }
 
 export default Routes;
 
-function ProtectedRoute() {
-  // If not signedIn redirect to `/signIn` page
-  const isUserSignedIn = useUserContext().isAuth;
-  if (!isUserSignedIn) {
-    return <Navigate replace to='signIn' />;
-  }
-
+function SignedInLayout() {
   // Add layout componet for SignedIn page
   return (
     <>
