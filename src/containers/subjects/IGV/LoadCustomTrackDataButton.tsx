6d48b5a68e1b9dc9986f32cc7c9c81016d0e314b
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-10-11 16:26:15 +1100

    Subject IGV Page (#153)

---
+++ src/containers/subjects/IGV/LoadCustomTrackDataButton.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import { RequiredS3RowType } from './utils';
+
+type Props = {
+  handleAddCustomLoadTrack(newTrackData: RequiredS3RowType): void;
+};
+
+function LoadCustomTrackDataButton({ handleAddCustomLoadTrack }: Props) {
+  const [isAddCustomTrackDialogOpen, setIsAddCustomTrackDialogOpen] = useState<boolean>(false);
+
+  const [inputState, setInputState] = useState<RequiredS3RowType>({
+    bucket: 'production',
+    key: '',
+  });
+  const handleOpenCustomTrackButton = () => {
+    setInputState((prev) => ({ ...prev, key: '' }));
+    setIsAddCustomTrackDialogOpen((prev) => !prev);
+  };
+  const addNewTrackData = () => {
+    handleAddCustomLoadTrack(inputState);
+    setIsAddCustomTrackDialogOpen((prev) => !prev);
+  };
+
+  const renderFooter = () => {
+    return (
+      <div className='pt-4'>
+        <Button
+          label='Cancel'
+          icon='pi pi-times'
+          onClick={() => setIsAddCustomTrackDialogOpen((prev) => !prev)}
+          className='p-button-text text-blue-800'
+        />
+        <Button
+          className='bg-blue-800'
+          label='ADD'
+          icon='pi pi-plus'
+          onClick={() => addNewTrackData()}
+          autoFocus
+        />
+      </div>
+    );
+  };
+  return (
+    <>
+      <Dialog
+        header={`Add Custom Track`}
+        visible={isAddCustomTrackDialogOpen}
+        className='w-11'
+        draggable={false}
+        footer={renderFooter()}
+        onHide={() => setIsAddCustomTrackDialogOpen((prev) => !prev)}>
+        <>
+          <div className='mb-4 text-500 '>
+            To add a custom track, please enter S3 Key. Typically you get it by Copy S3 Path button
+            when browsing through Subject data.
+          </div>
+          <div className='my-3'>
+            <label htmlFor='username1' className='block mb-1'>
+              S3 Bucket Name
+            </label>
+            <Dropdown
+              disabled={true}
+              value={inputState.bucket}
+              options={[{ label: 'production', value: 'production' }]}
+              onChange={(e) => setInputState((prev) => ({ ...prev, bucket: e.value }))}
+              placeholder='Bucket Name'
+            />
+          </div>
+          <div className='my-3'>
+            <label htmlFor='username1' className='block mb-1'>
+              S3 Path
+            </label>
+            <InputText
+              className='w-full block focus:border-blue-800'
+              style={{ boxShadow: 'var(--blue-800)' }}
+              value={inputState.key}
+              onChange={(e) => setInputState((prev) => ({ ...prev, key: e.target.value }))}
+            />
+          </div>
+        </>
+      </Dialog>
+
+      <Button
+        onClick={handleOpenCustomTrackButton}
+        className='m-1 bg-blue-800 border-blue-800'
+        label='CUSTOM'
+        icon='pi pi-plus'
+      />
+    </>
+  );
+}
+
+export default LoadCustomTrackDataButton;
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,124 +2,122 @@
 // that don't have their own type declarations
 
 declare module 'igv' {
+  // NOTE: this is a VERY minimal typings definition copied from
+  // https://github.com/datavisyn/igv_wrapper/blob/master/typings/index.d.ts
+  // It does not look like the IGV authors are interested in keeping their own, and I don't want to make
+  // a comprehensive one in case it gets out of date
+  // https://github.com/igvteam/igv.js/issues/1376
+  // For the moment, some basic IGV types suit our use cases
 
-    // NOTE: this is a VERY minimal typings definition copied from
-    // https://github.com/datavisyn/igv_wrapper/blob/master/typings/index.d.ts
-    // It does not look like the IGV authors are interested in keeping their own, and I don't want to make
-    // a comprehensive one in case it gets out of date
-    // https://github.com/igvteam/igv.js/issues/1376
-    // For the moment, some basic IGV types suit our use cases
+  export function setOauthToken(any, any): void;
+  export function createBrowser(any, any): Promise<IGVBrowser>;
+  export function removeBrowser(any): void;
+  export function removeAllBrowsers(): void;
 
-    export function setOauthToken(any, any): void;
-    export function createBrowser(any, any): Promise<IGVBrowser>;
-    export function removeBrowser(any): void;
+  export interface IGVBrowser {
+    loadTrack(config: ITrack): Promise<any>;
+    removeTrackByName(name: string): void;
+    loadGenome(config: any): Promise<any>;
 
-    export interface IGVBrowser  {
-        loadTrack(config: ITrack): Promise<any>;
-        removeTrackByName(name: string): void;
-        loadGenome(config: any): Promise<any>;
+    /**
+     * Search by annotation symbol
+     * @param {string} locusOrGene
+     */
+    search(locusOrGene: string): void;
 
-        /**
-         * Search by annotation symbol
-         * @param {string} locusOrGene
-         */
-        search(locusOrGene: string): void;
+    /**
+     * Zoom in by a factor of 2
+     */
+    zoomIn(): void;
 
-        /**
-         * Zoom in by a factor of 2
-         */
-        zoomIn(): void;
+    /**
+     * Zoom out by a factor of 2
+     */
+    zoomOut(): void;
+  }
 
-        /**
-         * Zoom out by a factor of 2
-         */
-        zoomOut(): void;
-    }
+  export interface ITrack {
+    /**
+     * Track type  No default. If not specified, type is inferred from file format
+     */
+    type?: 'annotation' | 'wig' | 'alignment' | 'variant' | 'seg';
+    /**
+     * Type of data source.
+     * @default file
+     */
+    sourceType?: 'file' | 'ga4gh' | 'htsget';
+    /**
+     * File format  No default. If not specified format is inferred from file name extension
+     */
+    format?: string;
+    /**
+     * Display name (label). Required
+     */
+    name: string;
+    /**
+     * URL to the track data resource, such as a file or webservice. Required
+     */
+    url: string;
+    /**
+     * optional URL to a file index, such as a BAM .bai, Tabix .tbi, or Tribble .idx file.
+     */
+    indexURL?: string;
+    /**
+     * Flag used to indicate if a file is indexed or not. If indexURL is provided this flag is redundant, its main purpose is to indicate that a file is not indexed.
+     */
+    indexed?: boolean;
+    /**
+     * Integer value specifying relative order of track position on the screen. To pin a track to the bottom use Number.MAX_VALUE. If no order is specified, tracks appear in order of their addition.
+     */
+    order?: number;
+    /**
+     * CSS color value for track features, e.g. "#ff0000" or "rgb(100,0,100)"
+     */
+    color?: string;
+    /**
+     * Initial height of track viewport in pixels
+     * @default 50
+     */
+    height?: number;
+    /**
+     * If true, then track height is adjusted dynamically, within the bounds set by minHeight and maxHeight, to accomdodate features in view
+     * @default true;
+     */
+    autoHeight?: boolean;
+    /**
+     * Minimum height of track in pixels  50
+     * @default 50
+     */
+    minHeight?: number;
+    /**
+     * Maximum height of track in pixels
+     * @default 500
+     */
+    maxHeight?: number;
+    /**
+     * Maximum window size in base pairs for which indexed annotations or variants are displayed  1 MB for variants, 30 KB for alignments, whole chromosome for other track types
+     */
+    visibilityWindow?: number;
 
-    export interface ITrack {
-        /**
-         * Track type  No default. If not specified, type is inferred from file format
-         */
-        type?: 'annotation' | 'wig' | 'alignment' | 'variant' | 'seg';
-        /**
-         * Type of data source.
-         * @default file
-         */
-        sourceType?: 'file' | 'ga4gh' | 'htsget';
-        /**
-         * File format  No default. If not specified format is inferred from file name extension
-         */
-        format?: string;
-        /**
-         * Display name (label). Required
-         */
-        name: string;
-        /**
-         * URL to the track data resource, such as a file or webservice. Required
-         */
-        url: string;
-        /**
-         * optional URL to a file index, such as a BAM .bai, Tabix .tbi, or Tribble .idx file.
-         */
-        indexURL?: string;
-        /**
-         * Flag used to indicate if a file is indexed or not. If indexURL is provided this flag is redundant, its main purpose is to indicate that a file is not indexed.
-         */
-        indexed?: boolean;
-        /**
-         * Integer value specifying relative order of track position on the screen. To pin a track to the bottom use Number.MAX_VALUE. If no order is specified, tracks appear in order of their addition.
-         */
-        order?: number;
-        /**
-         * CSS color value for track features, e.g. "#ff0000" or "rgb(100,0,100)"
-         */
-        color?: string;
-        /**
-         * Initial height of track viewport in pixels
-         * @default 50
-         */
-        height?: number;
-        /**
-         * If true, then track height is adjusted dynamically, within the bounds set by minHeight and maxHeight, to accomdodate features in view
-         * @default true;
-         */
-        autoHeight?: boolean;
-        /**
-         * Minimum height of track in pixels  50
-         * @default 50
-         */
-        minHeight?: number;
-        /**
-         * Maximum height of track in pixels
-         * @default 500
-         */
-        maxHeight?: number;
-        /**
-         * Maximum window size in base pairs for which indexed annotations or variants are displayed  1 MB for variants, 30 KB for alignments, whole chromosome for other track types
-         */
-        visibilityWindow?: number;
+    /**
+     * If true a "remove" item is included in the track menu.
+     */
+    removable?: boolean;
+    /**
+     * http headers to include with each request. For example {"foo": "fooValue", "bar": "barValue"}
+     */
+    headers?: any;
+    /**
+     * OAuth token, or function returning an OAuth token. The value will be included as a Bearer token with each request. See Oauth Support
+     */
+    oauthToken?: string | (() => string) | (() => Promise<string>);
 
-        /**
-         * If true a "remove" item is included in the track menu.
-         */
-        removable?: boolean;
-        /**
-         * http headers to include with each request. For example {"foo": "fooValue", "bar": "barValue"}
-         */
-        headers?: any;
-        /**
-         * OAuth token, or function returning an OAuth token. The value will be included as a Bearer token with each request. See Oauth Support
-         */
-        oauthToken?: string | (()=>string) | (() => Promise<string>);
+    // used for htsget - you can provide the endpoint and id as separate parts
+    id?: string;
+    endpoint?: string;
 
-        // used for htsget - you can provide the endpoint and id as separate parts
-        id?: string;
-        endpoint?: string;
-
-        squishedCallHeight?: number;
-        expandedCallHeight?: number;
-        displayMode?: string;
-    }
-
+    squishedCallHeight?: number;
+    expandedCallHeight?: number;
+    displayMode?: string;
+  }
 }
-
