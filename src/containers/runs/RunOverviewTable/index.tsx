11cc010c4699f217a68bc6a2bfb3388698db54c2
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-10-24 10:45:14 +1100

    dynamic run_id list

--- src/containers/runs/RunOverviewTable/index.tsx
+++ src/containers/runs/RunOverviewTable/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import JSONToTable from '../../../components/JSONToTable';
 import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
 import { usePortalRunsAPI } from '../../../api/run';
 import { useToastContext } from '../../../providers/ToastProvider';
 import CircularLoaderWithText from '../../../components/CircularLoaderWithText';
@@ -12,8 +13,10 @@ type Props = { runId: string };
 function RunOverviewTable({ runId }: Props) {
   const toast = useToastContext();
   const navigate = useNavigate();
+
+  const [inputRunId, setInputRunId] = useState<string>(runId);
   const runsQuery = usePortalRunsAPI({
-    queryStringParameters: { rowsPerPage: 1000 }, // TODO: make it dynamic or load more
+    queryStringParameters: { rowsPerPage: 250 },
   });
   const sequenceQuery = usePortalSequenceAPI({
     queryStringParameters: { instrument_run_id: runId },
@@ -40,15 +43,32 @@ function RunOverviewTable({ runId }: Props) {
   const sequenceInfo = sequenceResults[0];
   return (
     <div>
-      <div className='mb-5'>
-        <Dropdown
-          className='w-full'
-          value={runId}
-          options={runsResults}
-          onChange={(e) => navigate(`../../${e.target.value}`)}
-          virtualScrollerOptions={{ itemSize: 38 }}
-          placeholder='Select Run'
-        />
+      <div className='mb-3'>
+        <div className='flex'>
+          <Dropdown
+            className='flex-1'
+            value={inputRunId}
+            options={runsResults}
+            onChange={(e) => setInputRunId(e.value)}
+            virtualScrollerOptions={{ itemSize: 38 }}
+            editable
+            placeholder='Select Run'
+          />
+          <Button
+            onClick={() => navigate(`../../${inputRunId}`)}
+            icon='pi pi-search'
+            label='Search'
+            className='mx-2 p-button-outlined p-button-secondary'
+          />
+        </div>
+
+        <div id='helper-text' className='block p-2 text-500'>
+          <div className='font-semibold inline'>*</div>
+          <div className='font-normal inline'>
+            run_id dropdown list will only show the last 250 runs. Otherwise, copy and paste run_id
+            to the field above.
+          </div>
+        </div>
       </div>
 
       {sequenceQuery.data && <JSONToTable objData={sequenceInfo} />}
