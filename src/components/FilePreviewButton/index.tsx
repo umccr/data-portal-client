f08dd366b2697e6ff565d701191fec2af2afae4f
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2023-02-16 17:29:40 +1100

    css

--- src/components/FilePreviewButton/index.tsx
+++ src/components/FilePreviewButton/index.tsx
@@ -93,7 +93,7 @@ function FilePreviewDialog(props: FilePreviewDialogProps) {
       style={{ width: '75vw', boxSizing: 'border-box', border: 'solid var(--surface-600) 1px' }}
       visible={true}
       onHide={handleDialogClose}
-      contentStyle={{ minHeight: '5rem', maxHeight: '75vh' }}
+      contentStyle={{ minHeight: '10rem', maxHeight: '75vh' }}
       contentClassName='relative p-0 surface-400 flex align-items-center justify-content-center'>
       {portalPresignedUrlRes.data ? (
         <div className='w-full p-3' style={{ height: '75vh' }}>
