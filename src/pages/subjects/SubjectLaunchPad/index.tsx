0dbce17f011096cf5d38e6f54c942052e0ebde45
Victor San Kho Lin <victor@sankholin.com>
2024-09-18 20:35:38 +1000

    Disabled GPL trigger from Launch Pad
    Resolves #336

--- src/pages/subjects/SubjectLaunchPad/index.tsx
+++ src/pages/subjects/SubjectLaunchPad/index.tsx
@@ -4,7 +4,7 @@ import { Card } from 'primereact/card';
 
 import { Button } from 'primereact/button';
 
-import SubjectGPLLaunch from '../../../containers/subjects-launch/SubjectGPLLaunch';
+// import SubjectGPLLaunch from '../../../containers/subjects-launch/SubjectGPLLaunch';
 import SubjectRNAsumLaunch from '../../../containers/subjects-launch/SubjectRNAsumLaunch';
 import SubjectWGSTNLaunch from '../../../containers/subjects-launch/SubjectWGSTNLaunch';
 // import SubjectLaunchStarAlign from '../../../containers/subjects-launch/SubjectWTSStarAlignment';
@@ -13,7 +13,7 @@ import SubjectWGSTNLaunch from '../../../containers/subjects-launch/SubjectWGSTN
 export enum launchPadOptions {
   NONE = '',
   RNASUM = 'rna-sum',
-  GPL = 'gpl',
+  // GPL = 'gpl',
   WGS_TN = 'wgs-tn',
   STAR_ALIGN = 'star-align',
   ONCOANALYSER = 'oncoanalyser',
@@ -26,7 +26,8 @@ export default function SubjectLaunchPad() {
   }
 
   const launchPadLabel = [
-    { key: launchPadOptions.GPL, label: 'GRIDSS PURPLE LINX (GPL)' },
+    // See https://github.com/umccr/data-portal-client/issues/336
+    // { key: launchPadOptions.GPL, label: 'GRIDSS PURPLE LINX (GPL)' },
     { key: launchPadOptions.RNASUM, label: 'RNAsum' },
     { key: launchPadOptions.WGS_TN, label: 'Whole-Genome Sequencing Tumor-Normal (WGS T/N)' },
     // TODO de-scope from Ocicat release and let temporary hide them for now
@@ -45,33 +46,36 @@ export default function SubjectLaunchPad() {
         <div className='relative'>
           <div className='pt-2'>
             {/* What to trigger? */}
-            {lastPath == launchPadOptions.GPL ? (
-              <SubjectGPLLaunch subjectId={subjectId} />
-            ) : lastPath == launchPadOptions.RNASUM ? (
-              <SubjectRNAsumLaunch subjectId={subjectId} />
-            ) : lastPath == launchPadOptions.WGS_TN ? (
-              <SubjectWGSTNLaunch subjectId={subjectId} />
-            ) : (
-              // ) : lastPath == launchPadOptions.STAR_ALIGN ? (
-              //   <SubjectLaunchStarAlign subjectId={subjectId} />
-              // ) : lastPath == launchPadOptions.ONCOANALYSER ? (
-              //   <SubjectLaunchOncoanalyser subjectId={subjectId} />
-              <div className='h-full'>
-                <div className='text-2xl font-medium mb-4'>{subjectId} - Report trigger</div>
+            {
+              //   lastPath == launchPadOptions.GPL ? (
+              //   <SubjectGPLLaunch subjectId={subjectId} />
+              // ) :
+              lastPath == launchPadOptions.RNASUM ? (
+                <SubjectRNAsumLaunch subjectId={subjectId} />
+              ) : lastPath == launchPadOptions.WGS_TN ? (
+                <SubjectWGSTNLaunch subjectId={subjectId} />
+              ) : (
+                // ) : lastPath == launchPadOptions.STAR_ALIGN ? (
+                //   <SubjectLaunchStarAlign subjectId={subjectId} />
+                // ) : lastPath == launchPadOptions.ONCOANALYSER ? (
+                //   <SubjectLaunchOncoanalyser subjectId={subjectId} />
+                <div className='h-full'>
+                  <div className='text-2xl font-medium mb-4'>{subjectId} - Report trigger</div>
 
-                {launchPadLabel.map((option) => {
-                  return (
-                    <div key={option.key} className='mb-3'>
-                      <Button
-                        onClick={() => navigate(option.key)}
-                        label={option.label}
-                        className='w-full p-button-outlined p-button-secondary'
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            )}
+                  {launchPadLabel.map((option) => {
+                    return (
+                      <div key={option.key} className='mb-3'>
+                        <Button
+                          onClick={() => navigate(option.key)}
+                          label={option.label}
+                          className='w-full p-button-outlined p-button-secondary'
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              )
+            }
           </div>
         </div>
       </Card>
